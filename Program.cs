c4d1ba889bce27743784d784795ee6dcd485beba
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-08-23 14:19:05 +0800

    Task done

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿void Output(int[]? array)
+{
+    for (int i = 0; i < array.Length; ++i)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+
+void Input(int[]? array)
+{
+    for (int i = 0; i < 8; ++i)
+        array[i] = int.Parse(Console.ReadLine());
+}
+
+int[] array = new int[8];
+Input(array);
+
+Console.WriteLine();
+
+Output(array);
